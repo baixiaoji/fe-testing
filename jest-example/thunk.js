55c92494e6a52cd62aa3bb24dbe1e4ae94d1925a
baixiaoji <1041874421@qq.com>
2018-04-04 17:56:54 +0800

    [feat] 增强了thunk的功能

--- jest-example/thunk.js
+++ jest-example/thunk.js
@@ -4,10 +4,20 @@
  * @param {*} size 
  */
 export default function chunk(array,size=1){
+    
+    if(!(array instanceof Array)){
+        throw new Error('You need pass an array')
+    }
+
     const time = Math.ceil(array.length / size)
+    if(time === 1){
+        return array
+    }
     const newArray = []
     for(let i = 0; i < time; i++){
         newArray.push(array.splice(0,size))
     }
     return newArray
 }
+
+
--- jest-example/thunk.test.js
+++ jest-example/thunk.test.js
@@ -8,4 +8,17 @@ describe('achieve the loadash thunk function', () => {
     test("add some example size",() => {
         expect(thunk([2,1,3,1,3],2)).toEqual([[2,1],[3,1],[3]])
     })
+
+    test("the size is a negative number", () => {
+        expect(thunk([1,2,3], -1)).toEqual([])
+    })
+
+    test("the size greater than array size", () => {
+        expect(thunk([1,2,3], 4)).toEqual([1,2,3])
+    })
+
+    test("the first arguments is not a array", () => {
+        expect(() => {thunk(null, 1)}).toThrow()
+        expect(() => {thunk(null, 1)}).toThrowError('You need pass an array')
+    })
 })
